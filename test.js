d3d49511b6ace3d05a8df96f3d87bcc7dcc5058f
nicegegegit <112197127@qq.com>
2021-01-02 20:25:27 +0800

    test dev

--- test.js
+++ test.js
@@ -2,6 +2,11 @@ var testJS={
     init:function(){
         console.log("hello come here");
     },
+
+    log:funciton(s){//add dev
+    	console.log(s);
+    },
+
 	destory:function(){
 		console.log("destory");
 	}
