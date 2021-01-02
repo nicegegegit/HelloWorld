9a0f5eab1bbfd5dffbb5e5a2b4ed52190a0605a3
nicegegegit <112197127@qq.com>
2021-01-02 21:23:11 +0800

    mod file

--- dev/test.js
+++ dev/test.js
@@ -2,6 +2,9 @@ var testJS={
     init:function(){
         console.log("hello come here");
     },
+    dev：function(s){//add dev
+    	console.log(s);
+    },
 	destory:function(){
 		console.log("destory");
 	}
